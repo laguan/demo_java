527a704e401badca3f99186eb20bd90907aef16d
laguan <15386660167@163.com>
2023-04-28 20:34:16 +0800

    3

--- demo1/src/main/java/com/example/demo1/asd.java
+++ demo1/src/main/java/com/example/demo1/asd.java
@@ -1,7 +1,12 @@
 package com.example.demo1;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class asd {
+    @RequestMapping("/zxc")
+    public String asd(){
+        return "zxc";
+    }
 }
