850961dcbeecc781c0ce834c40e9083861053600
laguan <15386660167@163.com>
2023-04-28 20:51:17 +0800

    新建程序入口

--- demo1/src/main/java/com/example/demo1/asd.java
+++ demo1/src/main/java/com/example/demo1/asd.java
@@ -1,12 +1,18 @@
 package com.example.demo1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@EnableAutoConfiguration
 @RestController
 public class asd {
     @RequestMapping("/zxc")
     public String asd(){
         return "zxc";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(asd.class,args);
     }
 }
