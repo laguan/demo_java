22379b7acbea766326ab1e8c561036deebf1b8fa
laguan <15386660167@163.com>
2023-04-28 19:52:05 +0800

    spring

---
+++ mayikt/src/test/java/com/mayikt/service/MayiktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayikt.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayiktApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
