845ea2a32632aa1d602f5260e48db8eb32bd9f9c
laguan <15386660167@163.com>
2023-04-28 20:06:05 +0800

    第一次提交

--- demo2/src/main/java/com/demo2/service/Demo2Application.java
+++ demo2/src/main/java/com/demo2/service/Demo2Application.java
@@ -1,13 +1,13 @@
-package com.example.demo_01;
+package com.demo2.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo01Application {
+public class Demo2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo01Application.class, args);
+        SpringApplication.run(Demo2Application.class, args);
     }
 
 }
--- demo2/src/main/java/com/demo2/service/asd.java
+++ demo2/src/main/java/com/demo2/service/asd.java
@@ -1,2 +1,12 @@
-package com.demo2.service;public class asd {
+package com.demo2.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class asd {
+    @RequestMapping("/asd")
+    public String asd(){
+        return "zxc";
+    }
 }
